95abb7359cc844602267b67d2ad4caea846c0ce1
Duncan Ndiithi <duncanndiithi@yahoo.com>
2022-02-22 23:09:59 +0300

    add site pages templates

---
+++ src/app/pages/item-groups/item-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-groups',
+  templateUrl: './item-groups.component.html',
+  styleUrls: ['./item-groups.component.css']
+})
+export class ItemGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
